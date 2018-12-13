df756ca70b14533eeb1fd862a9f8768e2c15563f
Peer Xu <pppeerxu@gmail.com>
2018-12-13 14:58:29 +0800

    :tada: add StreamingEcho

--- pkg/echo/service/echo.go
+++ pkg/echo/service/echo.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/any"
+	component_pb "github.com/nayotta/metathings/pkg/proto/component"
 
 	pb "github.com/nayotta/metathings-component-echo/proto"
 )
@@ -38,6 +39,59 @@ func (self *EchoService) HANDLE_GRPC_Echo(ctx context.Context, in *any.Any) (*an
 
 func (self *EchoService) Echo(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
 	return &pb.EchoResponse{Text: req.GetText().GetValue()}, nil
+}
+
+type StreamingEchoServerWrapper struct {
+	component_pb.ModuleService_StreamCallServer
+}
+
+func (self *StreamingEchoServerWrapper) Send(res *pb.EchoResponse) error {
+	res_val, err := ptypes.MarshalAny(res)
+	if err != nil {
+		return err
+	}
+
+	out := &component_pb.StreamCallResponse{
+		Response: &component_pb.StreamCallResponse_Data{
+			Data: &component_pb.StreamCallDataResponse{
+				Value: res_val,
+			},
+		},
+	}
+
+	err = self.ModuleService_StreamCallServer.Send(out)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (self *StreamingEchoServerWrapper) Recv() (*pb.EchoRequest, error) {
+	var in pb.EchoRequest
+
+	req, err := self.ModuleService_StreamCallServer.Recv()
+	if err != nil {
+		return nil, err
+	}
+
+	req_val := req.GetData().GetValue()
+
+	err = ptypes.UnmarshalAny(req_val, &in)
+	if err != nil {
+		return nil, err
+	}
+
+	return &in, nil
+}
+
+func NewStreamingEchoServerWrapper(upstm component_pb.ModuleService_StreamCallServer) pb.EchoService_StreamingEchoServer {
+	return &StreamingEchoServerWrapper{upstm}
+}
+
+func (self *EchoService) HANDLE_GRPC_StreamingEcho(upstm component_pb.ModuleService_StreamCallServer) error {
+	return self.StreamingEcho(NewStreamingEchoServerWrapper(upstm))
 }
 
 func (self *EchoService) StreamingEcho(stm pb.EchoService_StreamingEchoServer) error {
